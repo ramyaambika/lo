bf0a99c9ec3242386b54839cb4b9b51865b45d7b
ramyaambika <43332585+ramyaambika@users.noreply.github.com>
2019-02-21 10:07:05 +0530

    Update arm.py

--- arm.py
+++ arm.py
@@ -1,10 +1,10 @@
-n=int(input())
+a=int(input())
 sum=0
-temp=n
-while(n!=0):
-  rem=n%10
+temp=a
+while(a!=0):
+  rem=a%10
   sum=sum+rem*rem*rem
-  n=n//10
+  a=a//10
 if (temp==sum):
   print("yes")
 else:
